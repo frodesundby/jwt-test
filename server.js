9da71991990ac72afbbdf78a7418774717e69d3f
Frode Sundby <frodesun@gmail.com>
2016-12-06 20:34:04 +0100

    Legger til Informasjons"slides" og fungerende server.js

--- server.js
+++ server.js
@@ -1,32 +1,29 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const morgan = require('morgan')
+const expressJWT = require("express-jwt")
+const jwt = require("jsonwebtoken")
 
 let idiots = ["Vegans", "Hipsters", "Lobsters", "Kanye West"]
 
 const app = express()
 
 app.use(morgan("combined"))
-app.use(bodyParser.json({type: '*/*'}))
+app.use(bodyParser.json())
+app.use(expressJWT({secret: "balls 4 ever"}).unless({path: ["/login", "/idiots"]}))
 
 app.get("/idiots", (req, res) => {
     res.send(idiots)
 })
 
-app.put("/idiots", (req, res) => {
+app.put("/idiot", (req, res) => {
     idiots.push(req.body.idiot)
     res.send(req.body.idiot + " added")
 })
 
-
-app.delete("/idiots", (req, res) => {
-    const index = idiots.indexOf(req.body.idiot)
-    if (index > -1){
-        idiots.splice(index, 1)
-        res.send(req.body.idiot + " removed")
-    } else {
-        res.send(req.body.idiot + " not found")
-    }
+app.post("/login", (req, res) => {
+    const token = jwt.sign({username: "bob", permissions:"loads"}, "balls 4 ever")
+    res.send(token)
 })
 
 app.listen(4242, () => console.info("Up and atom!"))
